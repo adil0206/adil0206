995372c9c71cbe049d5e39fe7758264b078126f6
ahdeielna <abdul.adil.hyd@gmail.com>
2021-12-15 19:30:39 +0530

    modified ...kuldeep

---
+++ ContactDetails/src/com/app/controller/HomeController.java
@@ -0,0 +1,7 @@
+package com.app.controller;
+
+public class HomeController {
+	public static void main(String[] args) {
+		System.out.println("Kuldeep");
+	}
+}
--- ContactDetails/src/com/app/model/Employee.java
+++ ContactDetails/src/com/app/model/Employee.java
@@ -5,4 +5,41 @@ public class Employee {
 	private String name;
 	private String email;
 	private String address;
+	@Override
+	public String toString() {
+		return "Employee [id=" + id + ", name=" + name + ", email=" + email + ", address=" + address + "]";
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public Employee(int id, String name, String email, String address) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.email = email;
+		this.address = address;
+	}
+	
+	
 }
